06f89cac4a39f57770535d1b11f4e74847079510
sign4l <jdalmau@gmail.com>
2019-04-30 19:00:32 +0200

    Componentes agregados ng g c component/nombre

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { GameFormComponent } from './components/game-form/game-form.component';
+import { GameListComponent } from './components/game-list/game-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    GameFormComponent,
+    GameListComponent
   ],
   imports: [
     BrowserModule,
